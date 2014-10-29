81a6a044e3d53575adda9a8b68c75b12b299ae98
Alexander Ferguson <x@echonotation.com>
2014-10-29 08:11:07 -0700

    Add files

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.login import LoginManager
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+db = MongoEngine()
+db.init_app(app)
+
+lm = LoginManager()
+lm.init_app(app)
+
+from capp import views
